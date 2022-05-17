936bec6e352065adba1fc95caad4a42adde98704
Linda <lindashen3@gmail.com>
2022-05-17 00:51:34 -0400

    Store question and options in an array

--- src/components/UserQuestions.js
+++ src/components/UserQuestions.js
@@ -2,28 +2,77 @@ import Navigation from "./Navigation";
 import { useState } from "react";
 
 function UserQuestions() {
-    const [question, setQuestion] = useState(0);
+    const [questionIndex, setQuestionIndex] = useState(0);
 
-    const questions = [
-        'What is your name?',
-        'What programming language are you most interested in?',
-        'What industry are you most interested in working in?',
-        'What resources would you like to see to facilitate building your project?',
-        'How long do you want to spend on this project?'
+    const questionsArr = [
+        {
+            question: 'What is your name?',
+            options: null
+        },
+        {
+            question: 'What programming language are you most interested in?',
+            options: [
+                'JavaScript',
+                'Python',
+                'C#',
+                'Ruby'
+            ]
+        },
+        {
+            question: 'What industry are you most interested in working in?',
+            options: [
+                'B2B Software and Services',
+                'Retail',
+                'Gaming',
+                'Healthcare',
+                'Education'
+            ]
+        },
+        {
+            question: 'What resources would you like to see to facilitate building your project?',
+            options: [
+                'YouTube Videos',
+                'Completed Project Samples ',
+                'Links to Relevant Courses'
+            ]
+        },
+        {
+            question: 'How long do you want to spend on this project?',
+            options: [
+                '<3 Months',
+                '3-6 months',
+                '6-9 months',
+                '>9 months'
+            ]
+        }
     ]
+
+    const navigateNext = () => {
+        if (questionIndex < 4) {
+            setQuestionIndex(questionIndex + 1);
+        }
+    }
+
+    const navigateBack = () => {
+        if (questionIndex > 0) {
+            setQuestionIndex(questionIndex - 1);
+        }
+    }
 
     return(
         <div>
             <Navigation />
                 <div className="question-container wrapper">
-                    <p>Question #{question + 1}</p>
-                    <h1>{questions[question]}</h1>
+                    <p>Question #{questionIndex + 1}</p>
+                    <h1>{questionsArr[questionIndex].question}</h1>
+                    
                     <form>
                         <input type="text" id="name" name="name" className="name-input"/>
                     </form>
+                    
                     <div className="nav-btn">
-                        <button className="btn-back">Back</button>
-                        <button className="btn-primary btn-next">Next</button>
+                        <button className="btn-back" onClick={navigateBack}>Back</button>
+                        <button className="btn-primary btn-next" onClick={navigateNext}>Next</button>
                     </div>
                 </div>
         </div>
