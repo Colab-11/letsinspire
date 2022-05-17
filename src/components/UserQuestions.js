19495cd966f0ac44ea4c4a5543717a2b1dd26ba1
Linda <lindashen3@gmail.com>
2022-05-17 00:17:58 -0400

    feat: add user questions component

--- src/components/UserQuestions.js
+++ src/components/UserQuestions.js
@@ -1,8 +1,32 @@
+import Navigation from "./Navigation";
+import { useState } from "react";
+
 function UserQuestions() {
+    const [question, setQuestion] = useState(0);
+
+    const questions = [
+        'What is your name?',
+        'What programming language are you most interested in?',
+        'What industry are you most interested in working in?',
+        'What resources would you like to see to facilitate building your project?',
+        'How long do you want to spend on this project?'
+    ]
+
     return(
-        <>
-            <p>Question start</p>
-        </>
+        <div>
+            <Navigation />
+                <div className="question-container wrapper">
+                    <p>Question #{question + 1}</p>
+                    <h1>{questions[question]}</h1>
+                    <form>
+                        <input type="text" id="name" name="name" className="name-input"/>
+                    </form>
+                    <div className="nav-btn">
+                        <button className="btn-back">Back</button>
+                        <button className="btn-primary btn-next">Next</button>
+                    </div>
+                </div>
+        </div>
     )
 }
 
