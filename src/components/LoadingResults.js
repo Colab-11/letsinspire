97f475052427f6b31db37135049d71931feebf8b
Linda <lindashen3@gmail.com>
2022-05-23 00:30:17 -0400

    fix button active class

--- src/components/LoadingResults.js
+++ src/components/LoadingResults.js
@@ -2,6 +2,7 @@ const LoadingResults = () => {
     return (
         <div className="loading-results wrapper">
             <h1>Finding Your Project</h1>
+            <div className="spinner"></div>
         </div>
     )
 }
